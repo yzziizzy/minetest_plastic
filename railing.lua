1dafb882ac6009754e4f8bfefb74c846e4e845e3
Izzy <yzziizzy@gmail.com>
2013-10-18 10:23:19 -0700

    railing mods

--- railing.lua
+++ railing.lua
@@ -1,7 +1,7 @@
  
 
 
-minetest.register_node("plastic:railing_z", {
+minetest.register_node("plastic:railing_1", {
 	description = "Plastic Railing",
 	-- drawtype = "allfaces_optional",
 	tiles = { "plastic_plastic_block.png" },
@@ -25,13 +25,49 @@ minetest.register_node("plastic:railing_z", {
 	
 })
 
+minetest.register_node("plastic:railing_2", {
+	description = "Plastic Railing",
+	-- drawtype = "allfaces_optional",
+	tiles = { "plastic_plastic_block.png" },
+	paramtype = "light",
+	groups = {cracky=2},
+	sounds = default.node_sound_stone_defaults(),
+	drawtype = "nodebox",
+	node_box = {
+		type = "fixed",
+		fixed = {
+			{-0.1, 0.3, -0.5, 0.1, 0.5, 0.5}, -- rail
+			{-0.20, -0.5, -0.20, 0.25, 0.4, 0.25}, -- posts, all fucked up, no time to fiddle
+			{-0.50, -0.5, -0.50, 0.55, 0.4, 0.55}, -- posts
+			{-0.70, -0.5, -0.70, 0.75, 0.4, 0.75}, -- posts
+		},
+	},
+	selection_box = {
+		type = "fixed",
+		fixed = {
+			{-0.1, -0.5, -0.5, 0.1, 0.5, 0.5}
+		},
+	},
+	
+})
+
 
 minetest.register_craft( {
-	output = "plastic:railing_z",
+	output = "plastic:railing_1",
 	recipe = {
 			{ "homedecor:plastic_sheeting", "homedecor:plastic_sheeting", "homedecor:plastic_sheeting" },
 			{ "", "homedecor:plastic_sheeting", "" },
 			{ "", "homedecor:plastic_sheeting", "" },
+
+	},
+})
+			
+minetest.register_craft( {
+	output = "plastic:railing_2",
+	recipe = {
+			{ "homedecor:plastic_sheeting", "homedecor:plastic_sheeting", "homedecor:plastic_sheeting" },
+			{ "homedecor:plastic_sheeting", "", "homedecor:plastic_sheeting"},
+			{ "homedecor:plastic_sheeting", "", "homedecor:plastic_sheeting"},
 
 	},
 })
